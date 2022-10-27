ad086822630628ec268d60d1dfdf4ebb73d5bc62
Denis <denischen242@gmail.com>
2022-10-27 16:53:33 -0400

    changed setup for math functions

--- general.py
+++ general.py
@@ -1,6 +1,7 @@
-from pycalculator import calculate
+from pycalculator import calculate,numerize,solve
 
 def main():
-    print(calculate('9^(1/(1+2))^6'))
+    #print(calculate('9^(1/(1+2))^6'))
+    print(numerize('sin(3)'))
     
 main()
